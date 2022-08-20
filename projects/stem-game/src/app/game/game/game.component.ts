a0c16e926def418d4b0b97044462d33baebc46a5
ivokraev <ivo.kraev2@protonmail.com>
2022-08-20 16:53:02 +0300

    Updated game menu page

--- projects/stem-game/src/app/game/game/game.component.ts
+++ projects/stem-game/src/app/game/game/game.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'stem-game-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    return;
   }
-
 }
