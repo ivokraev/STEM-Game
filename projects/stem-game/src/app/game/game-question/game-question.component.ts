18c607b0c400543c3ef14094b2f4fd754a0391fc
ivokraev <ivo.kraev2@protonmail.com>
2022-08-26 19:44:17 +0300

    Added logic for displaying question

--- projects/stem-game/src/app/game/game-question/game-question.component.ts
+++ projects/stem-game/src/app/game/game-question/game-question.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription, tap } from 'rxjs';
+import { IGameQuestion } from '../../shared/models/game/game-question.model';
+
+import { selectCurrentQuestion } from '../store/selectors/game.selectors'
 
 @Component({
   selector: 'stem-game-game-question',
   templateUrl: './game-question.component.html',
   styleUrls: ['./game-question.component.scss']
 })
-export class GameQuestionComponent implements OnInit {
+export class GameQuestionComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  currentQuestion: IGameQuestion | null = null;
+  currentQuestionSub!: Subscription;
+
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
+    this.currentQuestionSub = this.store.select(selectCurrentQuestion).pipe(
+      tap((data: IGameQuestion | null) => this.currentQuestion = data)
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.currentQuestionSub.unsubscribe();
   }
 
 }
